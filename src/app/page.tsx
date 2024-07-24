8bbea564a7bf7462c27bc67c7cdbd180f871a468
KAP220 <88947023+KAP220@users.noreply.github.com>
2024-07-24 16:29:04 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+
+export default function Home() {
+  return (
+    <><h1 className="text-red-400">Welcome to DocChat , upload your document to chat with it </h1>
+    <Button>chat</Button></>
+  );
+}
